7c8bb8f5bcecd58e620dd9bb2e89380a97afcbec
KseniyaGunbina <senyagunbina@yandex.ru>
2022-06-11 21:50:59 +0300

    Корректирование записи final-переменных

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,8 @@ import java.util.concurrent.RecursiveTask;
 import java.util.stream.IntStream;
 
 public class Main {
-    static final int MIN = 0;
-    static final int MAX = 300000;
+    public static final int MIN = 0;
+    public static final int MAX = 300000;
 
     public static void main(String[] args) {
         int[] arr = IntStream.range(MIN, MAX).toArray();
@@ -22,10 +22,11 @@ public class Main {
         now = System.currentTimeMillis();
         int start = 0;
         int end = arr.length;
-        final Integer middle = (end - start) / 2;
-        RecursiveTask<Integer> task1 = new ArraySum(start, middle, arr);
-        RecursiveTask<Integer> task2 = new ArraySum(middle, end, arr);
-        RecursiveTask.invokeAll(task1, task2);
+
+        final Integer middle = (end - start) / 2; // делим массив пополам
+        RecursiveTask<Integer> task1 = new ArraySum(start, middle, arr); // поток для подсчета суммы первой половины
+        RecursiveTask<Integer> task2 = new ArraySum(middle, end, arr); // поток для подсчета суммы второй половины
+        RecursiveTask.invokeAll(task1, task2); // пул двух потоков
         finish = System.currentTimeMillis();
         System.out.printf("Сумма всех значений: %s. Время многопоточного решения %s мс\n", task1.join() + task2.join(), finish - now);
     }
